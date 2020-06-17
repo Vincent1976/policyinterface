87aa469246847295c769fd5d87a8de4c8935e8e8
仟 洪 <2420711278@qq.com>
2020-06-17 14:18:38 +0800

    修改数据库配置

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 DIALECT = 'mysql+pymysql'
 DRIVER = 'mysqldb'
 USERNAME = 'root'
-HOST = '120.133.142.144' #'124.70.184.203'
-PORT = 15336 #3306
-PASSWORD = 'sate1llite' #'7rus7U5!'
+HOST = '124.70.184.203'
+PORT = 3306
+PASSWORD = '7rus7U5!'
 DATABASE = 'policydata'
 SQLALCHEMY_DATABASE_URI = DIALECT + '://' + USERNAME + ':' + PASSWORD + '@' + HOST + ':' + str(PORT) + '/' + DATABASE
 SQLALCHEMY_TRACK_MODIFICATIONS = False
