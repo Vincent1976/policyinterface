7bf922eea5c22cd78c2d46bab01a82d738fa8031
Vincent1976 <jiangxinmin@163.com>
2020-07-27 10:16:17 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -507,7 +507,7 @@ def jmpolicy():
             exMessage += "packagequantity不能为空;"
 
         #单据唯一性
-        remotedata = policy_model.jm_ht_remotedata.query.filter(policy_model.jm_ht_remotedata.appkey==postdata['appkey'], policy_model.jm_ht_remotedata.channelOrderId==postdata['sequencecode']).order_by(policy_model.jm_ht_remotedata.CreateDate.desc()).all()
+        remotedata = policy_model.jm_ht_remotedata.query.filter(policy_model.jm_ht_remotedata.appkey==postdata['appkey'], policy_model.jm_ht_remotedata.Status=='投保成功', policy_model.jm_ht_remotedata.channelOrderId==postdata['sequencecode']).order_by(policy_model.jm_ht_remotedata.CreateDate.desc()).all()
         print(remotedata)
         result = []
         dataresult = model_to_dict(remotedata)
