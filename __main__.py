af79455a3b0738196a58b71aa04040b263a4ece7
zmm2019 <z18621900279@126.com>
2020-08-14 11:11:26 +0800

    钱江沙师弟日期加30天

--- __main__.py
+++ __main__.py
@@ -649,7 +649,7 @@ def issueInterface(guid):
         insuranceObject['amount'] = '12000.0' 
         insuranceObject['rate'] = str(decimal.Decimal(row[68][:-1]) * 10) # policyRate 去除百分号后乘以10 [:-1] 截取从头开始到倒数第一个字符之前
         insuranceObject['effectiveTime'] = row[36] # 保险起期 departDateTime           
-        insuranceObject['terminalTime'] = str(datetime.datetime.strptime(insuranceObject['effectiveTime'],'%Y-%m-%d %H:%M:%S')+ datetime.timedelta(days = 15)) # 上面时间+15天
+        insuranceObject['terminalTime'] = str(datetime.datetime.strptime(insuranceObject['effectiveTime'],'%Y-%m-%d %H:%M:%S')+ datetime.timedelta(days = 30)) # 上面时间+30天
         insuranceObject['copy'] = '1' # 份数 
         insuranceObject['docType'] = '' # 不必填
         insuranceObject['docSN'] = '' # 不必填
